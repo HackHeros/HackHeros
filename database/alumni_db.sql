aeeacccd98341b32daefc90afa7f34a6bdec85d3
ShaikhMudassir7 <send2mudassir@gmail.com>
2023-08-26 01:01:56 +0530

    Added whole db schema for admin side as well as user side

--- database/alumni_db.sql
+++ database/alumni_db.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Oct 16, 2020 at 05:17 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
